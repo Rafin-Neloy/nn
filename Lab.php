872bf72ce301e1bf1dad9e16c65bcf1a24d38320
Rafin Neloy <syedrafinbinmomin@gmail.com>
2021-07-13 09:07:38 +0600

    first commit

---
+++ Lab.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Conversion_site</title>
+</head>
+<body>
+<h3>Page First [home]</h3>
+
+	<h3>Conversion site</h3>
+
+
+<a href="login_form_action.php">1.home</a>     <a href="login_form_action.php">2.Conversion Rate</a>  <a href="login_form_action.php">3.History</a>
+
+
+
+<h3>Converter:</h3>
+
+
+<label for="Converter"></label>
+	<select id="Converter" >
+
+<option value="Feet to inch">Feet to inch</option>
+
+	 </select>
+
+
+	<form action="registration_form_action.php" method="POST">
+		<label for="fullname">Full Name: </label>
+		<input type="text" name="fullname" id="fullname">
+
+		<br><br>
+
+		<label for="username">Username: </label>
+		<input type="text" name="username" id="username">
+
+		<br><br>
+
+		<label for="password">Password: </label>
+		<input type="password" name="password" id="password">
+
+		<br><br>
+
+		<input type="submit" name="submit" value="Register">
+	</form>
+	
+	<br>
+	<a href="login_form_action.php">Go to Login</a>
+
+</body>
+</html>
